53723f3962715f366171a096cc7885a7a6b9e42f
Absamad <asry03327@gmail.com>
2024-08-23 18:27:47 +0300

    data type Object

---
+++ 05_Object.dart
@@ -0,0 +1,15 @@
+void main() {
+  /*
+  When you declare a variable as Object, you explicitly set the type of the variable to Object. 
+  Since Object is the root of all classes in Dart, it can hold any type of value.
+  */
+
+  Object x = 42; // x can hold any value because it's of type Object
+  x = 'hello'; // This is allowed because Object can hold any type
+  print(x is int); //false
+  print(x.length); // Error: The getter 'length' isn't defined for the class 'Object'.
+
+  if (x is String) {
+    print(x.length); // Allowed after checking x is a String
+  }
+}
