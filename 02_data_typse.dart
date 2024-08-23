5b29c928189775e4cff484acbb6c7d6f773d30b5
Absamad <asry03327@gmail.com>
2024-08-23 18:25:29 +0300

    update 02 and 03

--- 02_data_typse.dart
+++ 02_data_typse.dart
@@ -8,13 +8,27 @@ void main() {
   print('Is single : $single');
   print('have money $money\$');
 
-  // var It could be any variable
-  var s = "Ahmed"; // s is string
-  var x = 25; // x is int
-  //x = "string";  error x is int  , we can't change
+  /*
+  var It could be any variable
+  the type is inferred by the compiler based on the assigned value.
+  Once the type is inferred, it cannot change.
+  */
+  
+
+  var s = "Ahmed";
+  print(s.length);
+
+  var x = 25; 
+  x = 50;  // Allowed because 50 is also an int
+  //x = "Hello";  // Error: A value of type 'String' can't be assigned to a variable of type 'int'
+  var foo = const []; // The type of foo is inferred as List<dynamic> because it is being assigned an empty list.
+
+  foo = const [1, 2, 3]; // Reassigning to another const list is allowed.
+  //foo.add(4); // Error: Cannot add to an unmodifiable list.
 
   String? name1; // Nullable type. Can be `null` or string.
 
   String name2; // Non-nullable type. Cannot be `null` but can be string.
-  //print(name2); error
+  //print(name2); //error
+
 }
--- 03_Final_and_const.dart
+++ 03_Final_and_const.dart
@@ -1,11 +1,17 @@
 void main()
 {
+  //a final variable can only be set once.
   final String ? name;
   name = "yousef";    //Can be set at runtime.
   print(name);
 
   final String name1 = "yousef"; 
   print(name1);
+
+  // You can declare a final variable with or without a type annotation.
+  //the type is inferred
+  final dog = 'Bob';
+
 
 /*
   error
@@ -18,4 +24,10 @@ void main()
   const String last = "Ahmed"; 
   print(last);
 
+
+  //Since the value is an integer, Dart infers the type as int.
+  const bar = 1000000;
+
+  const baz = [];
+  //baz = [42]; // Error: Constant variables can't be assigned a value.
 }
