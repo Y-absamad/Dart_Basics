e759971f2b6a66bd3a6eb2b3217dd412d9013212
Absamad <asry03327@gmail.com>
2024-08-24 11:00:59 +0300

    fixed error

--- 05_Object.dart
+++ 05_Object.dart
@@ -7,7 +7,7 @@ void main() {
   Object x = 42; // x can hold any value because it's of type Object
   x = 'hello'; // This is allowed because Object can hold any type
   print(x is int); //false
-  print(x.length); // Error: The getter 'length' isn't defined for the class 'Object'.
+  //print(x.length); // Error: The getter 'length' isn't defined for the class 'Object'.
 
   if (x is String) {
     print(x.length); // Allowed after checking x is a String
