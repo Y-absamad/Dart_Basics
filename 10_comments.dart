4c3d72dd23d4173d855dbbf3496bdea85a4f3787
Absamad <asry03327@gmail.com>
2024-08-24 15:13:16 +0300

    comments in dart

---
+++ 10_comments.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  // Single-line comments
+
+  /*
+    Multi-line comments
+
+    A multi-line comment begins with /* and ends with */. 
+    Everything between /* and */ is ignored by the Dart compiler
+
+  */
+
+  /// Documentation comments
+  /// Inside a documentation comment, the analyzer ignores all text unless it is enclosed in brackets.
+  /// so don't forget to [feed] them some [Food].
+ 
+}
